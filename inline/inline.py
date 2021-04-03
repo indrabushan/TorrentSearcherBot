0fde6578b66daf36b0fe2bc3e68d2adb29bc33bd
indrabushan <72606082+indrabushan@users.noreply.github.com>
2021-04-03 14:05:07 +0530

    Update inline.py

--- inline/inline.py
+++ inline/inline.py
@@ -8,81 +8,6 @@ from telegram import (
 from config import FOOTER_TEXT
 
 from helpers.torrent import torrent_search
-from commands.commands import torrent
-
-def inlinequery(update, context):
-    """Handles the inline query."""
-    query = update.inline_query.query
-    buttons = [[InlineKeyboardButton(text="Search Again", switch_inline_query_current_chat="")]]
-    if len(query) == 0:
-        results = [InlineQueryResultArticle(
-            id=uuid4(),
-            title="Search any keyword",
-            input_message_content=InputTextMessageContent(
-                f"*Search any torrent\nExample : *`Avengers`\n\n{FOOTER_TEXT}", 
-                parse_mode="Markdown"
-                ),
-            reply_markup=InlineKeyboardMarkup(buttons)
-            )
-        ]
-
-        context.bot.answer_inline_query(update.inline_query.id, results=results)
-        return 
-    print(len(query))
-    torrent = torrent_search(query)
-    results = []
-    
-    if torrent == None:
-        results.append(InlineQueryResultArticle(
-            id=uuid4(),
-            title="Search any keyword",
-            input_message_content=InputTextMessageContent(
-                f"*Search any torrent\nExample : *`Avengers`\n\n{FOOTER_TEXT}", 
-                parse_mode="Markdown"
-                ),
-            reply_markup=InlineKeyboardMarkup(buttons)
-            )
-        )
-        context.bot.answer_inline_query(update.inline_query.id, results=results)
-        return
-
-    if len(torrent) == 0:
-        results.append(InlineQueryResultArticle(
-            id=uuid4(),
-            title="404 Not Found",
-            input_message_content=InputTextMessageContent(
-                f"*fuck there is no results for your query {query}*\n\n{FOOTER_TEXT}", 
-                parse_mode="Markdown"
-                ),
-            reply_markup=InlineKeyboardMarkup(buttons)
-            )
-        )
-        context.bot.answer_inline_query(update.inline_query.id, results=results)
-        return
-
-
-    for response in torrent[:15]:
-        name = response.get("name")
-        age = response.get("age")
-        leechers = response.get("leecher")
-        magnet_link = response.get("magnet")
-        seeders = response.get("seeder")
-        size = response.get("size")
-        type_of_file = response.get("type")
-        site = response.get("site")
-        torrent_url = response.get("url")
-        results.append(InlineQueryResultArticle(
-            id=uuid4(),
-            title=name,
-            input_message_content=InputTextMessageContent(
-                f"*Name : {name}\nSize : {size}\nAge : {age}\nLeechers : {leechers}\nNo: of seeds : {seeders}\nType of File : {type_of_file}\nTorrent Url : {torrent_url}*\n\n*Magnet Link : *`{magnet_link}`\n\n*Powered by {site} website*\n\n{FOOTER_TEXT}", 
-                parse_mode="Markdown"
-                ),
-            reply_markup=InlineKeyboardMarkup(buttons)
-            )
-        )
-
-    context.bot.answer_inline_query(update.inline_query.id, results=results)
 
 def button(update, context):
     query = update.callback_query
@@ -112,7 +37,7 @@ def button(update, context):
     type_of_file = response[0].get("type")
     site = response[0].get("site")
     torrent_url = response[0].get("url")
-    buttons = [[InlineKeyboardButton(text="Try InlineQuery", callback_data=torrent)]]
+    buttons = [[InlineKeyboardButton(text="Try InlineQuery", switch_inline_query="")]]
 
     query.edit_message_text(text=f"*Name : {name}\nSize : {size}\nAge : {age}\nLeechers : {leechers}\nNo: of seeds : {seeders}\nType of File : {type_of_file}\nTorrent Url : {torrent_url}*\n\n*Magnet Link : *`{magnet_link}`\n\n*Powered by {site} website*\n\n{FOOTER_TEXT}", parse_mode="Markdown", reply_markup=InlineKeyboardMarkup(buttons))
 
