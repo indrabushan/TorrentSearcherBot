03eb203d4a1a8103e0ff046d54ff1e84a66f1966
SI_NN_ER_LS <72606082+si-nn-er-ls@users.noreply.github.com>
2023-12-14 08:56:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-TOKEN = os.environ.get("BOT_TOKEN")
-START_MESSAGE = os.environ.get("START_MESSAGE", "*Hi ! I am a simple torrent searcher made by my [SINNER](tg://user?id={686505963}).\n\n\nFor leeching torrent files join our channel [JOIN GROUP](buttonurl:https://t.me/Torrent_To_File_v2)*")
-FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "*Made with ❤️ by @SI_NN_ER_LS*")
+TOKEN = "BOT_TOKEN"
+START_MESSAGE = "START_MESSAGE", "*Hi ! I am a simple torrent searcher made by my [SINNER](tg://user?id={686505963}).\n\n\nFor leeching torrent files join our channel [JOIN GROUP](buttonurl:https://t.me/Torrent_To_File_v2)*"
+FOOTER_TEXT = "FOOTER_TEXT", "*Made with ❤️ by @SI_NN_ER_LS*"
